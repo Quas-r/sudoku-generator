f71555a823d9c28cda7c0cb534c585323aea2e3f
Bora <boraeroglu238@hotmail.com>
2024-04-05 02:14:21 +0300

    fix: change the sudoku generation algorythm to backtraching
    it still doesn't work. needs debugging.

--- sudoku.c
+++ sudoku.c
@@ -9,14 +9,14 @@
 #define SQR_DIM     3
 
 void display_grid(int grid[GRID_DIM][GRID_DIM]);
+int is_zero(int grid[GRID_DIM][GRID_DIM], int row, int column);
 int check_column(int grid[GRID_DIM][GRID_DIM], int value, int row, int column);
 int check_row(int grid[GRID_DIM][GRID_DIM], int value, int row, int column);
 int check_square(int grid[GRID_DIM][GRID_DIM], int value, int row, int column);
-int try_insert_into_point(int grid[GRID_DIM][GRID_DIM], int value, int row, int column);
-int try_insert_value(int grid[GRID_DIM][GRID_DIM], int value);
-void setup_grid(int grid[GRID_DIM][GRID_DIM]);
-int fill_grid(int grid[GRID_DIM][GRID_DIM], int numbers[]);
-void randomize_numbers(int numbers[]);
+int try_insert_value(int grid[GRID_DIM][GRID_DIM], int value, int row, int column);
+void clear_grid(int grid[GRID_DIM][GRID_DIM]);
+int fill_grid(int grid[GRID_DIM][GRID_DIM], int numbers[], int filled_cells);
+void shuffle(int numbers[]);
 void *thread(void* ptr);
 
 
@@ -31,23 +31,29 @@ int main(int argc, char *argv[])
 {
     srand(time(NULL));
 
-    pthread_t thread1, thread2, thread3, thread4, thread5, thread6, thread7, thread8;
-    pthread_create(&thread1, NULL, *thread, (void*) threads_done);
-    pthread_create(&thread2, NULL, *thread, (void*) threads_done);
-    pthread_create(&thread3, NULL, *thread, (void*) threads_done);
-    pthread_create(&thread4, NULL, *thread, (void*) threads_done);
-    pthread_create(&thread5, NULL, *thread, (void*) threads_done);
-    pthread_create(&thread6, NULL, *thread, (void*) threads_done);
-    pthread_create(&thread7, NULL, *thread, (void*) threads_done);
-    pthread_create(&thread8, NULL, *thread, (void*) threads_done);
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
-    pthread_join(thread3, NULL);
-    pthread_join(thread4, NULL);
-    pthread_join(thread5, NULL);
-    pthread_join(thread6, NULL);
-    pthread_join(thread7, NULL);
-    pthread_join(thread8, NULL);
+    int grid[GRID_DIM][GRID_DIM];
+    int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    clear_grid(grid);
+    fill_grid(grid, numbers, 0);
+    display_grid(grid);
+
+    // pthread_t thread1, thread2, thread3, thread4, thread5, thread6, thread7, thread8;
+    // pthread_create(&thread1, NULL, *thread, (void*) threads_done);
+    // pthread_create(&thread2, NULL, *thread, (void*) threads_done);
+    // pthread_create(&thread3, NULL, *thread, (void*) threads_done);
+    // pthread_create(&thread4, NULL, *thread, (void*) threads_done);
+    // pthread_create(&thread5, NULL, *thread, (void*) threads_done);
+    // pthread_create(&thread6, NULL, *thread, (void*) threads_done);
+    // pthread_create(&thread7, NULL, *thread, (void*) threads_done);
+    // pthread_create(&thread8, NULL, *thread, (void*) threads_done);
+    // pthread_join(thread1, NULL);
+    // pthread_join(thread2, NULL);
+    // pthread_join(thread3, NULL);
+    // pthread_join(thread4, NULL);
+    // pthread_join(thread5, NULL);
+    // pthread_join(thread6, NULL);
+    // pthread_join(thread7, NULL);
+    // pthread_join(thread8, NULL);
     return 0;
 }
 
@@ -77,10 +83,11 @@ void display_grid(int grid[GRID_DIM][GRID_DIM]) {
     }
 }
 
+int is_zero(int grid[GRID_DIM][GRID_DIM], int row, int column) {
+    return grid[row][column] == 0;
+}
+
 int check_column(int grid[GRID_DIM][GRID_DIM], int value, int row, int column) {
-    if (grid[row][column] != 0) {
-        return FALSE;
-    }
     for (int i = 0; i < GRID_DIM; i++) {
         if (i != row && grid[i][column] == value) { return FALSE; }
     }
@@ -88,9 +95,6 @@ int check_column(int grid[GRID_DIM][GRID_DIM], int value, int row, int column) {
 }
 
 int check_row(int grid[GRID_DIM][GRID_DIM], int value, int row, int column) {
-    if (grid[row][column] != 0) {
-        return FALSE;
-    }
     for (int i = 0; i < GRID_DIM; i++) {
         if (i != column && grid[row][i] == value) { return FALSE; }
     }
@@ -98,9 +102,6 @@ int check_row(int grid[GRID_DIM][GRID_DIM], int value, int row, int column) {
 }
 
 int check_square(int grid[GRID_DIM][GRID_DIM], int value, int row, int column) {
-    if (grid[row][column] != 0) {
-        return FALSE;
-    }
     int row_sqr_num = row / SQR_DIM;
     int column_sqr_num = column / SQR_DIM;
     int i, j, row_index, column_index;
@@ -114,7 +115,7 @@ int check_square(int grid[GRID_DIM][GRID_DIM], int value, int row, int column) {
     return TRUE;
 }
 
-int try_insert_into_point(int grid[GRID_DIM][GRID_DIM], int value, int row, int column) {
+int try_insert_value(int grid[GRID_DIM][GRID_DIM], int value, int row, int column) {
     if (check_square(grid, value, row, column) &&
             check_row(grid, value, row, column) &&
             check_column(grid, value, row, column)) {
@@ -124,60 +125,43 @@ int try_insert_into_point(int grid[GRID_DIM][GRID_DIM], int value, int row, int 
     return FALSE;
 }
 
-int try_insert_value(int grid[GRID_DIM][GRID_DIM], int value) {
-    for (int i = 0; i < GRID_DIM; i++) {
-        for (int j = 0; j < GRID_DIM; j++) {
-            if (try_insert_into_point(grid, value, i, j)) { return TRUE; }
-        }
-    }
-    return FALSE;
-}
-
-void setup_grid(int grid[GRID_DIM][GRID_DIM]) {
+void clear_grid(int grid[GRID_DIM][GRID_DIM]) {
     for (int i = 0; i < GRID_DIM; i++) {
         for (int j = 0; j < GRID_DIM; j++) { grid[i][j] = 0; }
     }
 }
 
-int fill_grid(int grid[GRID_DIM][GRID_DIM], int numbers[]) {
-    int filled_cells = 0;
-    int attempts = 0;
-    int value;
-    while (filled_cells < total_cells) {
-        // randomize_numbers(numbers);
-        int changed = FALSE;
-        for (int i = 0; i < 9; i++) {
-            if (try_insert_value(grid, value)) {
-                filled_cells++;
-                changed = TRUE;
+int fill_grid(int grid[GRID_DIM][GRID_DIM], int numbers[], int filled_cells) {
+    int value, row, column;
+    if (filled_cells == total_cells) return TRUE;
+    for (int i = 0; i < total_cells; i++) {
+        row = i / 9;
+        column = i % 9;
+        if (is_zero(grid, row, column)) {
+            shuffle(numbers);
+            for (int j = 0; j < 9; j++) {
+                if (try_insert_value(grid, value, row, column)) {
+                    if (filled_cells == total_cells) { return TRUE; }
+                    if (fill_grid(grid, numbers, filled_cells + 1)) { return TRUE; }
+                }
             }
         }
-        if (!changed) return FALSE;
     }
-    putchar('\n');
-    return TRUE;
+    grid[row][column] = 0;
+    return FALSE;
 }
 
 void *thread(void* ptr) {
     int grid[GRID_DIM][GRID_DIM];
     int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    long threads_done_t = FALSE;
-    while (!threads_done) {
-        setup_grid(grid);
-        threads_done_t = fill_grid(grid, numbers);
-        if (threads_done_t) {
-            pthread_mutex_lock(&mutex);
-            threads_done = TRUE;
-            pthread_mutex_unlock(&mutex);
-            display_grid(grid);
-        }
-    }
+    fill_grid(grid, numbers, 0);
+    display_grid(grid);
     return 0;
 }
 
-void randomize_numbers(int numbers[]) {
+void shuffle(int numbers[]) {
     int x, y, z;
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 10; i++) {
         x = rand() % 9;
         y = rand() % 9;
         z = numbers[x];
